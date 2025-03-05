8a83cbb28b871cab6286a2de820d72a89021c8a0
NikolajF <cph-nf70@cphbusiness.dk>
2025-03-05 13:22:29 +0100

    Fixed the VictoryPoint/Gate. Animation works, colliders work, transition to menu works and you can re-enter the game, GameManager also created. It should later help save stats, gear ect.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,54 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     
+    // If necessary we can later make this a singleton for easier use
     private bool gameIsOver = false;
+
+    public string gameScene = "SampleScene";    // Handle target scene in inspector
+    
+    private void Start()
+    {
+        if (string.IsNullOrEmpty(gameScene))
+        {
+            Debug.LogError("Game scene name is not set!");
+        }
+    }
+    
+    private void Awake()
+    {
+        // We only want one instance
+        if (GameObject.FindWithTag("GameManager") != null && GameObject.FindWithTag("GameManager") != this.gameObject)
+        {
+            Destroy(this.gameObject);  // Remove duplicates
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);  // To save stats, gear and stuff we should have one GameMagager at all times
+        }
+    }
 
     public void EndRound()
     {
         if (gameIsOver) return;
 
         gameIsOver = true;
+
+        StartCoroutine(EndRoundCoroutine());
+    }
+
+    private IEnumerator EndRoundCoroutine()
+    {
+        // Potentially insert animation or screen fade here?
         
-        // Could later make some transition or effect here
+        yield return new WaitForSeconds(1f);
+
         SceneManager.LoadSceneAsync("Main Menu");
         
-        // Perhaps some score saving and gear saving logic here
+        // We'll need save score and/or gear logic here
     }
 
     public void StartNewRound()
@@ -23,8 +56,15 @@ public class GameManager : MonoBehaviour
         gameIsOver = false;
         
         // We should have reset logic here for player position, resetting puzzles ect.
-
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);     // Reload current scene
+        
+        if (!string.IsNullOrEmpty(gameScene))
+        {
+            SceneManager.LoadScene(gameScene);  // Load by scene name
+        }
+        else
+        {
+            Debug.LogError("Game scene name is not set!");
+        }
     }
 
 }
